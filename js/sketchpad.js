4f753b99c2411a1859af8c0f7013aa15520a4e17
Wendy <wendybeth010@gmail.com>
2014-07-03 20:34:22 -0400

    Add grid, mouseenter function, and color class

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+	for(var i=0; i<16; i++) {
+		$('table').append('<tr></tr>');
+		};
 
-});
+	for(var j=0; j<16; j++) {
+		$('tr').append('<td><div class="pixel"></div></td>');
+	};
+
+	$('.pixel').mouseenter(function() {
+		$(this).addClass('color');
+	});
+
+}); 
