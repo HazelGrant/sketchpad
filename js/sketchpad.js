f3e00ecab608f65754b7dfb25f9f25f14b1315fe
Wendy <wendybeth010@gmail.com>
2014-07-03 23:00:34 -0400

    Limited functionality

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -11,8 +11,26 @@ $(document).ready(function() {
 		$(this).addClass('color');
 	});
 
-	$('.button').click(function() {
-		$('.pixel').removeClass('color');
-	});
+	$('button').click(function() {
+		var table = document.getElementById("table");
+		table.remove();
 
+		$('.container').append('<table class="table"></table>');
+
+		var rows = prompt("Enter a number");
+		var cells = prompt("Enter another number");
+
+		for(var i=0; i<rows; i++) {
+			$('table').append('<tr></tr>');
+		};
+
+		for(var j=0; j<cells; j++) {
+			$('tr').append('<td><div class="pixel"></div></td>');
+		};
+
+		$('.pixel').mouseenter(function() {
+			$(this).addClass('color');
+		});
+
+	});
 }); 
